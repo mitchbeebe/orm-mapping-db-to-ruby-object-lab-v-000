20eaac108c6a2483d5e3e4e17f9a500a1b58aed9
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-12-16 20:52:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,11 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
-    
+    sql = <<-SQL
+      select *
+      from Students
+      where name = ?
+    SQL
   end
   
   def save
