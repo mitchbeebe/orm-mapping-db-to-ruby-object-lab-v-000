ac33b497783a71e5aa4bd42d03c924c1a15b34be
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-12-16 20:47:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,9 @@ class Student
   def self.new_from_db(row)
     # create a new Student object given a row from the database
     s = self.new
-    s.id = DB[:conn]
+    s.id = row[0]
+    s.name = row[1]
+    s.grade = row[2]
   end
 
   def self.all
