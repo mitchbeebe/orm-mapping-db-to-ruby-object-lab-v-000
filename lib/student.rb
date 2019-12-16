ac3ef72355a4614e4f5210e46e00a29afefa9774
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-12-16 21:20:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,7 @@ class Student
     self.all.select{|x| x.grade.to_i == 10}.first
   end
   
-  # def self.all_students_in_grade_X(X)
-  #   self.all.select{|x| x.grade.to_i == X}
-  # end
+  def self.all_students_in_grade_X(X)
+    self.all.select{|x| x.grade.to_i == X}
+  end
 end
